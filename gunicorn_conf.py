b7b263e5f139795482152fa03e918997209c8796
Soumadip <soumadipsinghamahapatra@gmail.com>
2025-04-02 17:53:08 +0530

    Update Gunicorn configuration to bind to default port and enable debug mode in Uvicorn

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -5,5 +5,4 @@ import multiprocessing
 workers = multiprocessing.cpu_count() * 2 + 1
 
 # Socket to bind
-bind = "0.0.0.0:8000"  # Render sets the PORT environment variable
 worker_class = "uvicorn.workers.UvicornWorker"  # Use Uvicorn worker for ASGI apps
--- main.py
+++ main.py
@@ -399,4 +399,4 @@ def send_email():
 
 if __name__ == '__main__':
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", debug=True)
